41640d7159e5173c61f9073adaa45870f48032de
fray517 <feden.stroy@mail.ru>
2026-01-11 13:17:30 +0300

    создана страница news

--- zerocoder/zerocoder/settings.py
+++ zerocoder/zerocoder/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "main",
+    "main", "news"
 ]
 
 MIDDLEWARE = [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
 TIME_ZONE = "UTC"
 
